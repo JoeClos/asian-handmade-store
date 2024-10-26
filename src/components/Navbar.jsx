9cd242caaa01ee39025c5ac52c16526d0e313101
nanoview <arif.js@gmail.com>
2024-10-26 20:09:09 +0300

    Style navbar material ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,94 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from "@mui/material/Typography";
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import CloseIcon from '@mui/icons-material/Close';
+
 const Navbar = () => {
-    return <div>
-        This is the Navbar.
-    </div>
-}
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            LOGO
+          </Typography>
+          <CloseIcon />
+      </Box>
+      <Divider />
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ display: 'flex', flexDirection: 'column' }}>
+            <ListItemText>Home</ListItemText>
+            <ListItemText>Contact</ListItemText>
+            <ListItemText>Cart</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar sx={{  justifyContent: 'space-between'}}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block", lg: "flex" }, gap: { lg: "1rem" } }}>
+          <Typography>Home</Typography>
+            <Typography>Contact</Typography>
+            <Typography>Cart</Typography>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: 240,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
+  );
+};
 
 export default Navbar;
